2376d8672f6504420e50a4248123da6fb5752ad0
s.izumo <s.izumo@tech.nflabs.jp>
2023-09-26 19:28:29 +0900

    update:キーワード判断に対応

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+services = ["amazon", "ドコモ", "楽天", "サイボウズ"]
+keywords = ["情報流出", "自動音声", "注意"]
+
+
 def get_rss():
     import feedparser
     url = "https://scan.netsecurity.ne.jp/rss/index.rdf"
     feed = feedparser.parse(url)
+    links = []
     for entry in feed.entries:
-        print("%s\n%s\n%s\n\n"%(entry.title, entry.updated,  entry.link))
-        if "ドコモ" in entry.title:
-            return entry
+        for service in services:
+            for keyword in keywords:
+                if service in entry.title and keyword in entry.title:
+                    if entry.link not in links:
+                        send_line_notification(
+                            msg=entry.title, link=entry.link)
+                        print("%s\n%s\n%s\n\n" %
+                              (entry.title, entry.updated,  entry.link))
+                        links.append(entry.link)
+
 
 def send_line_notification(msg, link):
     import requests
@@ -20,5 +32,5 @@ def send_line_notification(msg, link):
 
 if __name__ == '__main__':
     # main()
-    article = get_rss()
-    send_line_notification(msg=article.title, link=article.link)
+    get_rss()
+    # send_line_notification(msg=article.title, link=article.link)
